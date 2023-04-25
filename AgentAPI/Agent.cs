98f81c307bee694db0e8c0796e4eab9ddb84fe98
Fatima Zahra <fatima.bouharba@ivisgroup.com>
2023-04-25 10:15:40 +0200

    Code Cleaning

--- AgentAPI/Agent.cs
+++ AgentAPI/Agent.cs
@@ -13,7 +13,6 @@ namespace AgentAPI
         public string Name { get; set; }
         public int Id { get; set; }
         public string Version { get; set; }
-
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public Status Status { get; set; }
 
