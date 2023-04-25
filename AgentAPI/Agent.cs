fc0d1da65f84e00237e1f5a9406060c687cc5e61
Fatima Zahra <fatima.bouharba@ivisgroup.com>
2023-04-25 10:01:43 +0200

    Add Regex of the IP Address

--- AgentAPI/Agent.cs
+++ AgentAPI/Agent.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace AgentAPI
 {
@@ -7,6 +8,7 @@ namespace AgentAPI
         public string Os { get; set; }
         public DateTime LastKeepAlive { get; set; }
         public DateTime DateAdd { get; set; }
+        [RegularExpression(@"^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ErrorMessage = "IP not valid.")]
         public string Ip { get; set; }
         public string Name { get; set; }
         public int Id { get; set; }
